4ac503f0424e83731d0c4126fe60aeb1461599cc
chuckimthien <57587184+chuckimthien@users.noreply.github.com>
2023-03-09 14:12:44 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# weather-app-TCDev
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-yayuta)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  imports: [],
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+})
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
